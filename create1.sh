b87c2dc0904dd27fb3bd570fe3aa639dbc82ee05
towa1204 <fdsfgjkal@gmail.com>
2022-04-22 18:04:11 +0900

    update

--- create1.sh
+++ create1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# create1.shが置いてあるディレクトリをカレントディレクトリとする
+cd `dirname $0`
 #配列
 a=(A B C D)
-echo "~/compro からのディレクトリを作成しA.cpp ~ D.cppを生成します."
-echo "~/compro/ からのファイル名を入力してください:"
+echo "A.cpp ~ D.cppを生成します."
+echo "~コンテスト名を入力してください:"
 read file
-mkdir ~/compro/${file}
+mkdir ${file}
 for ((i = 0; i < ${#a[@]}; i++))
 do
-    cp ~/compro/helloworld.cpp ~/compro/${file}/${a[i]}.cpp
+    cp ./helloworld.cpp ./${file}/${a[i]}.cpp
 done
